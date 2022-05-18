681aef646fbe3995573b961c476e4e053a73e949
xiaolu-coding <454819375@qq.com>
2022-05-18 14:45:11 +0800

    chore: 添加待办事项

--- src/components/ToDoApp.vue
+++ src/components/ToDoApp.vue
@@ -1,12 +1,22 @@
 <script lang="ts" setup>
-  const todos = [
-    {
-      id: 1,
-      text: 'Learn Vue.js 3',
-      completed: false
-    }
-  ]
- 
+import { reactive, ref } from "vue"
+
+let newTodo = ref("")
+let todos = reactive([
+  {
+    id: 1,
+    text: "Learn Vue.js 3",
+    completed: false,
+  },
+])
+
+const createTodo = () => {
+  todos.push({
+    id: 2,
+    text: newTodo.value,
+    completed: false,
+  })
+}
 </script>
 
 <template>
@@ -14,9 +24,10 @@
     <div v-for="todo in todos" :key="todo.id" data-test="todo">
       {{ todo.text }}
     </div>
+    <form data-test="form" @submit.prevent="createTodo">
+      <input data-test="new-todo" v-model="newTodo" />
+    </form>
   </div>
 </template>
 
-<style scoped lang="sass">
-
-</style>
+<style scoped lang="sass"></style>
--- src/components/__tests__/ToDoApp.spec.ts
+++ src/components/__tests__/ToDoApp.spec.ts
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
 import ToDoApp from "../ToDoApp.vue"
+import exp from 'constants'
 
 describe('测试ToDoApp组件', () => {
   it('render a todo', () => {
@@ -11,4 +12,18 @@ describe('测试ToDoApp组件', () => {
     // 断言元素的text文本是如此
     expect(todo.text()).toBe("Learn Vue.js 3")
   })
+
+  it('测试添加todo', async () => {
+    const wrapper = mount(ToDoApp)
+    // 此时的todo只有一个
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1)
+    // 输入框输入New todo
+    await wrapper.get('[data-test="new-todo"]').setValue('New todo')
+    // 表单触发submit事件
+    await wrapper.get('[data-test="form"]').trigger('submit')
+
+    expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
+  })
 })
+
+
